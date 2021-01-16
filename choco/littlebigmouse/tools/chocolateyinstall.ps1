285d81869dc032a84c65e2a3af0275aba7898e3a
Elias Benbourenane <eliasbenbourenane@gmail.com>
2021-01-16 19:10:01 +0400

    Added choco Little Big Mouse source

---
+++ choco/littlebigmouse/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = "Stop";
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = "exe"
+  url            = "https://github.com/mgth/LittleBigMouse/releases/download/4.2.7124.42685/LittleBigMouse_4.2.7124.42685.exe"
+  softwareName   = "Little Big Mouse*"
+  checksum       = "5760B36B506DE39CBF8855F3947C57BD742175FED7560023E5D42639CE280D8A"
+  checksumType   = "sha256"
+  silentArgs     = "/VERYSILENT"
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
